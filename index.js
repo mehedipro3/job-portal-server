29831f49fc22458ec84c32fe73c99ec521c2d967
mehedipro3 <mehediratul37@gmail.com>
2025-04-08 15:13:36 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+const logger = (req, res, next) => {
+  console.log('insider the logger');
+  next();
+}
+
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    if (err) {
+      return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    res.user = decode;
+    next();
+  })
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ee44r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,12 +60,35 @@ async function run() {
     const jobApplicationCollection = client.db('job-portal').collection('job_application');
 
 
+    //Auth related APIs
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+      })
+        .send({ success: true });
+    })
+     
+
+    //Token delete 
+
+    app.post('/logout', (req, res) => {
+      res.clearCookie('token', {
+        httpOnly: true,
+        secure: false,
+      })
+        .send({ success: true })
+    })
+
     //job related APIs
-    app.get("/job", async (req, res) => {
+    app.get("/job", logger, async (req, res) => {
       const email = req.query.email;
       let query = {};
-      if(email){
-        query = {hr_email : email};
+      if (email) {
+        query = { hr_email: email };
       }
       const result = await jobsCollection.find(query).toArray();
       res.send(result);
@@ -50,40 +103,45 @@ async function run() {
 
 
 
-    app.post("/job",async(req,res) =>{
+    app.post("/job", async (req, res) => {
       const newJob = req.body;
       const result = await jobsCollection.insertOne(newJob);
       res.send(result);
     })
 
-    app.get('/job-application/job/:job_id', async (req,res) => {
-       const jobId = req.params.job_id;
-       const query = {job_id : jobId};
-       const result = await jobApplicationCollection.find(query).toArray();
-       res.send(result);
+    app.get('/job-application/job/:job_id', async (req, res) => {
+      const jobId = req.params.job_id;
+      const query = { job_id: jobId };
+      const result = await jobApplicationCollection.find(query).toArray();
+      res.send(result);
     })
 
     //job-application apis
 
-    app.get("/job-application", async (req, res) => {
+    app.get("/job-application", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { application_email: email };
+
+      console.log('cuk cuk cookies', req.cookies);
+
+      if (res.user.email !== req.query.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+
       const result = await jobApplicationCollection.find(query).toArray();
 
       for (const application of result) {
         const query1 = { _id: new ObjectId(application.job_id) }
         const job = await jobsCollection.findOne(query1);
-        if(job){
-          application.title = job.title; 
-          application.company = job.company; 
-          application.applicationDeadline = job.applicationDeadline; 
-          application.company_logo = job.company_logo; 
-          application.location = job.location; 
+        if (job) {
+          application.title = job.title;
+          application.company = job.company;
+          application.applicationDeadline = job.applicationDeadline;
+          application.company_logo = job.company_logo;
+          application.location = job.location;
         }
       }
-
       res.send(result);
-
     })
 
     app.post("/job-applications", async (req, res) => {
@@ -93,21 +151,21 @@ async function run() {
     })
 
 
-    app.patch("/job-applications/:id", async (req,res) => {
+    app.patch("/job-applications/:id", async (req, res) => {
       const id = req.params.id;
       const data = req.body;
-      const filter = {_id : new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set :{
-            status : data.status
+        $set: {
+          status: data.status
         }
       }
-      const result = await jobApplicationCollection.updateOne(filter,updateDoc);
+      const result = await jobApplicationCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
 
 
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
